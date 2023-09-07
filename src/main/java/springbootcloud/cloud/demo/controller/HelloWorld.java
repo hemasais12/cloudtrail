15c1bac0b3ef70b7490dd21c8e3fd2179560373a
hemasais12 <115533356+hemasais12@users.noreply.github.com>
2023-09-07 16:32:26 +0530

    first commit

---
+++ src/main/java/springbootcloud/cloud/demo/controller/HelloWorld.java
@@ -0,0 +1,18 @@
+package springbootcloud.cloud.demo.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloWorld {
+
+    @RequestMapping("/hello")
+    public String sayhello(){
+        return "Hello world ! you are in";
+    }
+
+    @RequestMapping("/hello1")
+    public String sayhello1(){
+        return "Hello world ! you are in cloud as test 2";
+    }
+}
